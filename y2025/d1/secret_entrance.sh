a8a10d026fc9fb3b8f0be1dfeeb5416e0f92f9f0
Jelmm1019 <jelmer.stolwijk@gmail.com>
2025-12-01 20:14:12 +0100

    New: aoc day 1

---
+++ y2025/d1/secret_entrance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+n=50
+m=0
+while IFS="" read -r a || [ -n "$a" ]
+do
+    n=$(((n+(($(echo -n ${a:0:1} | od -iAn | tr -d ' ')&2)-1)*$(echo -n ${a:1:4}))%100))
+    m=$((m+!$n))
+done < $1
+echo $m
